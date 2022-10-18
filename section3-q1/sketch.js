1ffe1f634714c2cfc3a54d2183b2c709671c8b41
2131008h <2131008h@stu.kobe-u.ac.jp>
2022-10-18 11:51:31 +0900

    UpdateSection3-q1
    スペースを離すと不整脈になるようにしておきました。

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,6 +1,9 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let x;
+let lowTime;
+let maxTime;
 
 function setup(){
   createCanvas(200, 200);
@@ -8,9 +11,57 @@ function setup(){
   cycle = 100;
 }
 
+
+
+
 function draw(){
+
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+
+  //let size = count^2 + count*2 +1;
+ // ellipse(width / 2, height / 2, size );
+
+ if(keyIsDown(" ".charCodeAt(0))){
+  if(count < 95){background(160, 192, 255);
+  count = (count + 1) % cycle;
+  
+  let size = count*2; 
+  
+  ellipse(width / 2, height / 2, 50 );
+  }
+  else if(95<count <100){background(160, 192, 255);
+    count = (count + 1) % cycle;
+    
+     
+    
+    ellipse(width / 2, height / 2, 100 );
+
+  }
+ }
+ else{
+  background(160, 192, 255);
+  count = (count + 1) % cycle;
+  // BLANK[1]
+
+ let size = count^2 + count*2 +1;
+ ellipse(width / 2, height / 2, size );
+
+ }
+
+
+
+
+
 }
+
+
+ 
+
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+
